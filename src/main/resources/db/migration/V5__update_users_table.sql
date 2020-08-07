7872fbd5d4855ab6db9ba97f75b612a6ce8e32d1
Ativorn <p.tanvu@gmail.com>
2020-08-07 22:25:32 +0200

    modification v5

--- src/main/resources/db/migration/V5__update_users_table.sql
+++ src/main/resources/db/migration/V5__update_users_table.sql
@@ -5,5 +5,4 @@ ALTER TABLE `reservations`.`users`
     CHANGE COLUMN `lastname` `lastname`   VARCHAR(60)  NOT NULL,
     CHANGE COLUMN `login` `login`         VARCHAR(30)  NOT NULL,
     CHANGE COLUMN `password` `password`   VARCHAR(255) NOT NULL,
-    ADD UNIQUE INDEX `login_UNIQUE` (`login` ASC) VISIBLE;
-;
+    ADD UNIQUE(`login`);
